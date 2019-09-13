f84bc26ee57db620e055307fe6c37db41e060160
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2019-09-13 11:43:09 +0300

    Header component and routing is up

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  {
+    path: 'category',
+    loadChildren: './modules/category/category.module#CategoryModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
